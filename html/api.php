7861770eb21b462e626fcff2b9dff03bffe340b9
yury.tolochko <yury.tolochko@weavora.com>
2013-02-13 09:43:12 +0300

    ui api scratch

--- html/api.php
+++ html/api.php
@@ -9,13 +9,29 @@ include_once '../src/Weavora/QueryProfiler/Extension/ExecutionTimeExtension.php'
 include_once '../src/Weavora/QueryProfiler/Profile.php';
 include_once '../src/Weavora/QueryProfiler/Profiler.php';
 
+try {
 
-$connection = new \Weavora\QueryProfiler\Connection('localhost','sems_at_ser', 'root', 'root');
-$connection->connect();
+    if (empty($_REQUEST['host']) || empty($_REQUEST['database'])) {
+        throw new Exception('No database connection selected');
+    }
 
-$profiler = new \Weavora\QueryProfiler\Profiler();
-$profiler->setConnection($connection);
+    if (empty($_REQUEST['query'])) {
+        throw new Exception('Empty query');
+    }
 
-$profile = $profiler->profile("SELECT SQL_NO_CACHE * FROM keyword LIMIT 10");
+    $connection = new \Weavora\QueryProfiler\Connection($_REQUEST['host'],$_REQUEST['databse'],$_REQUEST['user'],$_REQUEST['password']);
+    $connection->connect();
 
-echo json_encode($profile);
+    $profiler = new \Weavora\QueryProfiler\Profiler();
+    $profiler->setConnection($connection);
+
+    $profile = $profiler->profile($_REQUEST['query']);
+
+    echo json_encode($profile);
+
+} catch (Exception $e) {
+    echo json_encode(array(
+        'error' => $e->getCode(),
+        'error_message' => $e->getMessage(),
+    ));
+}
